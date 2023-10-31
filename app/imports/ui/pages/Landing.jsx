18acdcfb1a8948ffbbc1f90ae8643a184ffb63db
narowetz <cnarowetz@gmail.com>
2023-10-30 21:40:18 -1000

    added files

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Calendar2CheckFill, FileEarmarkTextFill, PeopleFill } from 'react-bootstrap-icons';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+  <Container fluid={false}>
+    <Row>
+      <Col>
+        <Container className="d-flex align-items-center justify-content-center flex-column">
+          <PeopleFill className="mt-4" style={{ fontSize: '150px' }} />
+          <h1 style={{ textAlign: 'center' }}>Multiple Users</h1>
+          <p style={{ textAlign: 'center' }}>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</p>
+        </Container>
       </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
+      <Col>
+        <Container className="d-flex align-items-center justify-content-center flex-column">
+          <FileEarmarkTextFill className="mt-4" style={{ fontSize: '150px' }} />
+          <h1>Contact Details</h1>
+          <p style={{ textAlign: 'center' }}>For each contact, you can save their name, address, and phone number.</p>
+        </Container>
       </Col>
-
+      <Col>
+        <Container className="d-flex align-items-center justify-content-center flex-column">
+          <Calendar2CheckFill className="mt-4" style={{ fontSize: '150px' }} />
+          <h1>Timestamped Notes</h1>
+          <p style={{ textAlign: 'center' }}>Each time you make contact with a contact, you can write a note that summarizes the conversation. This note is saved along with a timestamp with the contact.</p>
+        </Container>
+      </Col>
     </Row>
   </Container>
 );
